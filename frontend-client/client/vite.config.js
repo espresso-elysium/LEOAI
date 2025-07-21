f112499d2427e475c53fd6a7e167ec0da42a724b
espresso-elysium <espressoelysium@gmail.com>
2025-07-21 13:54:41 +0530

    Update vite.config.js

--- frontend-client/client/vite.config.js
+++ frontend-client/client/vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-  },
-  // Required for SPA routing to work on Render
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-});
+})
