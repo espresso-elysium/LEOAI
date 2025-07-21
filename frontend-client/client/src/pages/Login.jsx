33a3a9cc36399675b736e48fd505885749cd3871
espresso-elysium <espressoelysium@gmail.com>
2025-07-21 16:07:42 +0530

    Update Login.jsx

--- frontend-client/client/src/pages/Login.jsx
+++ frontend-client/client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 // src/pages/Login.jsx
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 export default function Login() {
   const [email, setEmail] = useState("");
